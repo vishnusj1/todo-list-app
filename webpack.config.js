4ec23201fb681685a63ad8fc4367e15bb5c12c07
vishnusj <vishnusj12@gmail.com>
2021-10-19 15:37:18 +0530

    Add Babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,34 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        app: "./src/index.js",
-        // main: "./src/main.js",
-    },
-    mode: "development",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    devtool: "inline-source-map",
-    devServer: {
-        static: "./dist",
-        hot: true,
-    },
+  entry: {
+    app: './src/index.js',
+    // main: "./src/main.js",
+  },
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+    ],
+  },
 };
